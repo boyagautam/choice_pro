32f4058065ee5a5e40a89fca911f7965b5536cec
gautam <boyagautam@gmail.com>
2022-10-14 17:00:51 +0530

    Initial Commit

---
+++ choice/Model/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  choice
+//
+//  Created by Goutham Boya on 26/09/22.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+    let choice3: String
+    let choice3Destination: Int
+}
